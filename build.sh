0dc7751393e1b064a3c011485d0881f877fe3f7d
humzashahid <humzasaur@gmail.com>
2024-03-28 05:42:26 +0000

    use .mlb file in shell command instead of directly including import.sml

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mlton -default-ann 'allowFFI true' -link-opt '-lglfw3 -framework Cocoa -framework OpenGL -framework IOKit' -export-header export.h import.sml glad.c ffi-import.c
+mlton -default-ann 'allowFFI true' -link-opt '-lglfw3 -framework Cocoa -framework OpenGL -framework IOKit' -export-header export.h import.mlb glad.c ffi-import.c
